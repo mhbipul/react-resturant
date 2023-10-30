9099333cf149e923be621d46fa3f637de8c9d545
Bipul <mhbipul99bd@gmail.com>
2023-10-30 16:18:06 +0600

    contact section complete

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,18 +5,24 @@ import { Box, Typography } from "@mui/material";
 const About = () => {
   return (
     <Layout>
-      <Box sx={{ my: 15, textAlign: "center" ,p:2,
-      "& h4":{
-        fontWeight:'bold',
-        my:2,
-        fontSize:'2rem'
-      },
-      '& p':{
-        textAlign:'justify'
-      },
-      "@media (max-width:600px)":{
-        mt:0}
-      }}>
+      <Box
+        sx={{
+          my: 15,
+          textAlign: "center",
+          p: 2,
+          "& h4": {
+            fontWeight: "bold",
+            my: 2,
+            fontSize: "2rem",
+          },
+          "& p": {
+            textAlign: "justify",
+          },
+          "@media (max-width:600px)": {
+            mt: 0,
+          },
+        }}
+      >
         <Typography variant="h4">Welcome to my Resturant!</Typography>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus ad
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,62 @@
 import React from 'react'
 import Layout from '../components/Layout/layout'
+import { Box, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from '@mui/material';
+import SupportAgentIcon from '@mui/icons-material/SupportAgent';
+import EmailIcon from '@mui/icons-material/Email';
+import CallIcon from '@mui/icons-material/Call';
 
 const contact = () => {
   return (
     <Layout>
-        <h1>This is contact page</h1>
+        <Box>
+          <Typography variant='h4' sx={{
+            textAlign:"center" ,
+            my:"10px",
+            ml:"10px",
+            "& h4":{
+              fontWeight: "bold",
+              mb: 2
+            }
+            
+            }}>
+            Contact My Resturant
+          </Typography>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ducimus aspernatur necessitatibus laboriosam eaque in dolore tempora tenetur velit corporis corrupti, quidem minima ad maxime cupiditate sint id est. Tempore facilis asperiores suscipit? Facilis obcaecati ut minus ad excepturi atque optio!</p>
+        </Box>
+        <Box sx={{m:4,width:"600px",ml:5,
+         "@media (max-width:600px)":{
+          width:"300px"
+        }}}>
+          <TableContainer component={Paper}>
+            <Table aria-label='contact table'>
+              <TableHead>
+                <TableRow>
+                  <TableCell sx={{backgroundColor:"black",color:"white"}} align='center'> Contact Details </TableCell>
+                </TableRow>
+
+              </TableHead>
+              <TableBody>
+                <TableRow>
+                  <TableCell>
+                    <SupportAgentIcon sx={{color:'red',pt:1}} />180-88-99-66-00(tollfree)
+                  </TableCell>
+                  
+                </TableRow>
+                <TableRow>
+                <TableCell>
+                    <EmailIcon sx={{color:'skyblue',pt:1}} />mhbipul99bd@gmail.com
+                  </TableCell>
+                </TableRow>
+                <TableRow>
+                <TableCell>
+                    <CallIcon sx={{color:'green',pt:1}} />+8801956480877
+                  </TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+
+          </TableContainer>
+        </Box>
     </Layout>
   )
 }
